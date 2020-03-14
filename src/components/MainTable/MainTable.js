64bd286ff20f5cb38b9932024e0fb27104bfa74e
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-03-14 18:35:25 +0300

    feat: implement content of table

--- src/components/MainTable/MainTable.js
+++ src/components/MainTable/MainTable.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import shortid from 'shortid';
 import headerTable from '../HeaderTable/HeaderTable';
-// import getObjData from '../helpers/dataObject';
-// import faker from 'faker';
+import getObjData from '../helpers/dataObject';
+import faker from 'faker';
+
+const arrData = faker => {
+  let arr = [];
+  let counter = 1100;
+  while (counter > 0) {
+    counter--;
+    arr.push(getObjData(faker));
+  }
+  return arr;
+};
+const arrDataTable = arrData(faker);
 
 const MainTable = () => {
   return (
@@ -9,11 +21,35 @@ const MainTable = () => {
       <div className='main'>
         <div className='main-wrapper container'>
           <table className='table'>
-            <caption>Title of table</caption>
+            <caption>Personal data</caption>
+
             <thead>
               <tr>{headerTable()}</tr>
             </thead>
-            {}
+
+            <tbody>
+              {arrDataTable.map((cell, ind) => {
+                return (
+                  <tr className='row' key={shortid.generate()}>
+                    <th className='scope'>{ind}</th>
+                    <td>
+                      <img src={cell.avatar} alt='avatar' />
+                    </td>
+                    <td>{cell.firstName}</td>
+                    <td>{cell.lastName}</td>
+                    <td className='tel'>
+                      <a href={`tel:${cell.tel}`}>{cell.tel} </a>
+                    </td>
+                    <td className='job'>{cell.job}</td>
+                    <td>{cell.jobDescriptor}</td>
+                    <td>{cell.jobType}</td>
+                    <td>
+                      <a href={`${cell.url}`}>{cell.url}</a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
       </div>
--- src/components/helpers/dataObject.js
+++ src/components/helpers/dataObject.js
@@ -1,31 +1,37 @@
 const getObjData = faker => {
+  const avatar = faker.internet.avatar()
   const firstName = faker.name.firstName();
   const lastName = faker.name.lastName();
   const tel = faker.phone.phoneNumber();
   const job = faker.name.jobTitle();
   const jobDescriptor = faker.name.jobDescriptor();
   const jobType = faker.name.jobType();
+  const url = faker.internet.url()
 
   const objData = {
+    avatar,
     firstName,
     lastName,
     tel,
     job,
     jobDescriptor,
-    jobType
+    jobType,
+    url
   };
 
   return objData;
 };
 
 export const getHeaderTable = [
-  'Number',
-  'FirstName',
-  'LastName',
+  'Id',
+  'Avatar',
+  'First Name',
+  'Last Name',
   'Tel',
   'Job',
-  'JobDescriptor',
-  'JobType'
+  'Job Descriptor',
+  'Job Type',
+  'Url'
 ];
 
 export default getObjData;
